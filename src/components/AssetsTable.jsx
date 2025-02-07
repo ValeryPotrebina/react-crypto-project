8d525ce9c22d33e032e0d464a65ec5e05d30be36
ValeryPotrebina <valery_potrebina_2003@mail.ru>
2025-02-07 13:37:10 +0300

    added Table

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,3 +1,46 @@
+import { Table } from "antd";
+import {useCrypto} from '../context/crypto-context'
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ['descend'],
+  },
+  {
+    title: "Price, $",
+    dataIndex: "price",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
+
 export default function AssetsTable() {
-    return <div>122</div>
+    const {assets} = useCrypto()
+
+    const data = assets.map(a => ({
+        key: a.id,
+        name: a.name,
+        price: a.price,
+        amount: a.amount
+    }))
+
+  return (
+    <div style={{ marginTop: 100, width: "100%" }}>
+      <Table
+        pagination={false}
+        columns={columns}
+        dataSource={data}
+        showSorterTooltip={{
+          target: "sorter-icon",
+        }}
+      />
+    </div>
+  );
 }
